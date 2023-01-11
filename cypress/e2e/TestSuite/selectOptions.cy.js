ea91fb1b8035e91ae133e025b536bca60ba0bb35
unknown <bdkakde@rediffmail.com>
2023-01-11 08:52:49 +0530

    Modified TC1

--- cypress/e2e/TestSuite/selectOptions.cy.js
+++ cypress/e2e/TestSuite/selectOptions.cy.js
@@ -1,14 +1,19 @@
 describe('Select Options Test suite', () => {
   it('TC1', () => {
-    cy.visit('https://portal2.passportindia.gov.in/AppOnlineProject/user/RegistrationBaseAction?request_locale=en')
+    cy.visit('https://register.rediff.com/register/register.php?FormName=user_details')
 
+    let optn = 'Angola'
     //Select the option with the text 
-    cy.get('#dcdrLocation').select('Goa').should('have.value', '16')
-    cy.task('log','Selected dropdown value is Goa')
+    cy.get('#country').select(optn).should('have.value', 5)
+
+    cy.get('#country').select(optn).find(':selected').contains(optn)
+    
+    cy.get('#country').select(optn).find('option:selected').should('have.text', optn);
+    cy.log('Selected dropdown value is ' + optn)
 
     //Select the option with index
-    //cy.get("#dcdrLocation").select(16).invoke("val").should("eq", "Goa")
-
+    cy.get('#country').select("India")
+    cy.get(':nth-child(1) > [colspan="3"] > select').select(2).invoke("val").should("eq", "Agra")
   })
 
   it.skip('TC2', () => {
@@ -22,4 +27,5 @@ describe('Select Options Test suite', () => {
     })
 
   })
+
 })
